ced03fa839afcbab46ffa919634223461397d852
Mauro Isidoro Diliberto <mau.dilib@gmail.com>
2023-07-20 17:13:50 +0200

    initializing repo

---
+++ src/main/java/com/example/TuneMixer/Project/TuneMixerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.TuneMixer.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneMixerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneMixerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TuneMixer/Project/TuneMixerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TuneMixer.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneMixerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
