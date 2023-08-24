967132d833ac3fc97434f47091e22a0c26c64815
Lorenzo2211 <lorenzosantonocito01@gmail.com>
2023-08-24 16:01:55 +0200

    Adding dao and service for playlist

--- src/main/java/com/example/TuneMixer/Project/entity/Dao/PlaylistDao.java
+++ src/main/java/com/example/TuneMixer/Project/entity/Dao/PlaylistDao.java
@@ -1,7 +1,28 @@
 package com.example.TuneMixer.Project.entity.Dao;
 
+import com.example.TuneMixer.Project.entity.Playlist;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class PlaylistDao {
+    List<Playlist> PlaylistList = new ArrayList<>();
+
+    public void addPlaylist (Playlist p){
+        this.PlaylistList.add(p);
+    }
+    public void deletePlaylist(Playlist p){
+        this.PlaylistList.removeIf(i -> (i.getId()).equals(p.getId()));
+    }
+
+    public void updatePlaylist(Playlist p){
+        this.PlaylistList.removeIf(in -> in.getId().equals(p.getId()));
+        this.PlaylistList.add(p);
+    }
+
+    public List<Playlist> getPlaylistList(){
+        return this.PlaylistList;
+    }
 }
