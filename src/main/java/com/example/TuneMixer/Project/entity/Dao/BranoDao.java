ee32bbd2e84eb5d56080f17bc9788aa4294e091c
Mauro Isidoro Diliberto <mau.dilib@gmail.com>
2023-08-24 16:25:39 +0200

    add crud methods in artista and brano repositories and services

--- src/main/java/com/example/TuneMixer/Project/entity/Dao/BranoDao.java
+++ src/main/java/com/example/TuneMixer/Project/entity/Dao/BranoDao.java
@@ -1,7 +1,31 @@
 package com.example.TuneMixer.Project.entity.Dao;
 
+import com.example.TuneMixer.Project.entity.Artista;
+import com.example.TuneMixer.Project.entity.Brano;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class BranoDao {
+
+    private List<Brano> branoList = new ArrayList<>();
+
+    public void addBrano(Brano brano){
+        this.branoList.add(brano);
+    }
+
+    public void deleteBrano(Brano titoloBrano){
+        this.branoList.removeIf(brano -> brano.getTitolo().equals(titoloBrano));
+    }
+
+    public void updateBrano(Brano brano){
+        this.branoList.removeIf(brano1 -> brano1.getTitolo().equals(brano.getTitolo()));
+        this.branoList.add(brano);
+    }
+
+    public List<Brano> getBranoList(){
+        return this.branoList;
+    }
 }
--- src/main/java/com/example/TuneMixer/Project/entity/Service/BranoService.java
+++ src/main/java/com/example/TuneMixer/Project/entity/Service/BranoService.java
@@ -1,7 +1,35 @@
 package com.example.TuneMixer.Project.entity.Service;
 
+import com.example.TuneMixer.Project.entity.Brano;
+import com.example.TuneMixer.Project.entity.Dao.BranoDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BranoService {
+
+    private BranoDao branoDao;
+
+    @Autowired
+    public BranoService(BranoDao branoDao){
+        this.branoDao = branoDao;
+    }
+
+    public void addBrano(Brano brano){
+        branoDao.addBrano(brano);
+    }
+
+    public void deleteBrano(Brano brano){
+        branoDao.deleteBrano(brano);
+    }
+
+    public void updateBrano(Brano brano){
+        branoDao.updateBrano(brano);
+    }
+
+    public List<Brano> getBranoList(){
+        return branoDao.getBranoList();
+    }
 }
